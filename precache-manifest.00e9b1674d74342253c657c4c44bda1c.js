56e5bea34431790a60802db62ed21d852a1febba
adrian.briggs.victoria@gmail.com <adrian.briggs.victoria@gmail.com>
2020-09-26 17:15:45 +0800

    Updates

---
+++ precache-manifest.00e9b1674d74342253c657c4c44bda1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9fe1dc8c28356d2d96384ad6f84237",
+    "url": "/data-meister-trial-assignment/index.html"
+  },
+  {
+    "revision": "399515f5125c0f934203",
+    "url": "/data-meister-trial-assignment/static/css/main.6f0d5ffc.chunk.css"
+  },
+  {
+    "revision": "3cb3297428cab4b7e4ce",
+    "url": "/data-meister-trial-assignment/static/js/2.46b60b9b.chunk.js"
+  },
+  {
+    "revision": "c7d86f6d33304a2e455704dcd2f2d9cb",
+    "url": "/data-meister-trial-assignment/static/js/2.46b60b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399515f5125c0f934203",
+    "url": "/data-meister-trial-assignment/static/js/main.1988569a.chunk.js"
+  },
+  {
+    "revision": "db7dc5ce396cbfc78bbe",
+    "url": "/data-meister-trial-assignment/static/js/runtime-main.63bf293e.js"
+  }
+]);
